b414575dd3142a0dab456769a1b0ff02b02fbe5a
Henrique Martins <henrique_rr_martins@hotmail.com>
2020-12-07 18:57:13 -0300

    Add telaAtendimento.xhtml; Add AtendimentoBean.java;

--- src/main/java/controle/AtendimentoBean.java
+++ src/main/java/controle/AtendimentoBean.java
@@ -1,18 +1,20 @@
 package controle;
 
-import java.util.List;
+import java.util.Date;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import entidade.Paciente;
+import entidade.Processo;
 import entidade.Profissional;
 
 @ManagedBean(name = "AtendimentoBean")
 @RequestScoped
 public class AtendimentoBean {
-
+	
 	private Long 	id;
-	private String	data;
+	private Date	data = new Date();
 	private String	cid;
 	private String 	avaliacao;
 	private String	evolucao;
@@ -21,8 +23,90 @@ public class AtendimentoBean {
 	private String 	conduta;
 	
 	private Profissional profissional;
-	//private Paciente 	 paciente;
+	private Paciente     paciente;
+	private Processo	 processo;
+
 	
-	//private List<Processo> listaProcessos;
+	//GETTERS & SETTERS
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getCid() {
+		return cid;
+	}
+
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+
+	public String getAvaliacao() {
+		return avaliacao;
+	}
+
+	public void setAvaliacao(String avaliacao) {
+		this.avaliacao = avaliacao;
+	}
+
+	public String getEvolucao() {
+		return evolucao;
+	}
+
+	public void setEvolucao(String evolucao) {
+		this.evolucao = evolucao;
+	}
+
+	public String getPrescricao() {
+		return prescricao;
+	}
+
+	public void setPrescricao(String prescricao) {
+		this.prescricao = prescricao;
+	}
+
+	public String getObs() {
+		return obs;
+	}
+
+	public void setObs(String obs) {
+		this.obs = obs;
+	}
+
+	public String getConduta() {
+		return conduta;
+	}
+
+	public void setConduta(String conduta) {
+		this.conduta = conduta;
+	}
+
+	public Profissional getProfissional() {
+		return profissional;
+	}
+
+	public void setProfissional(Profissional profissional) {
+		this.profissional = profissional;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+	
 	
 }
