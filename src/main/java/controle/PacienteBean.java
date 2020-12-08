b0aa5d0228e2a7e5c45083f03e3d5dbc2e7431a4
zeekmarinho <marinhoadm1986@gmail.com>
2020-12-07 21:57:00 -0300

    Criação da tela de paciente

---
+++ src/main/java/controle/PacienteBean.java
@@ -0,0 +1,13 @@
+package controle;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "PacienteBean")
+@RequestScoped
+public class PacienteBean {
+	
+	
+
+
+}
