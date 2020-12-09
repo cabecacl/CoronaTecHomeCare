998ddddeae23d2b9dfbc8763c5636b7b6f83e5d7
Cabeca <cabeca.cl@gmail.com>
2020-12-09 19:51:20 -0300

    Ajustes

--- src/main/java/fachada/Fachada.java
+++ src/main/java/fachada/Fachada.java
@@ -5,395 +5,365 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
+import dao.AtendimentoDAO;
+import dao.AtendimentoDAOImplementacao;
+import dao.PacienteDAO;
+import dao.PacienteDAOImplementacao;
+import dao.ProfissionalDAO;
+import dao.ProfissionalDAOImplementacao;
 import entidade.Atendimento;
 import entidade.Paciente;
 import entidade.Processo;
 import entidade.Profissional;
 import util.JpaUtil;
 
-
 public class Fachada {
 
+	private PacienteDAO pacienteDAO;
+	private AtendimentoDAO atendimentoDAO;
+	private ProfissionalDAO profissionalDAO;
+
+	public Fachada() {
+		this.pacienteDAO = new PacienteDAOImplementacao();
+		this.atendimentoDAO = new AtendimentoDAOImplementacao();
+		this.profissionalDAO = new ProfissionalDAOImplementacao();
+	}
+
 //Inserir Paciente	
-	
-public boolean inserirPaciente(Paciente paciente) {
-	EntityManager ent = JpaUtil.getEntityManager();
-	EntityTransaction tx = ent.getTransaction();
-	
-//	Paciente existe = ent.find(Paciente.class, paciente.getCpf());
+	public boolean inserirPaciente(Paciente paciente) {
 
-//	if(existe != null) {
-	
-	tx.begin();
-	ent.merge(paciente);
-	tx.commit();
-	ent.close();
-	
-//	return true;
-//	}
-	return false;	
-
-}
+		Paciente pacienteExiste = this.pacienteDAO.pesquisarPaciente(paciente.getCpf());
+		
+		if(pacienteExiste != null) {
+			return false;
+		}else {
+			return this.pacienteDAO.inserirPaciente(paciente);
+		}
+	}
 
 //Alterar Paciente
 
-public boolean alterarPaciente(Paciente paciente) {
-	EntityManager ent = JpaUtil.getEntityManager();
-	EntityTransaction tx = ent.getTransaction();
-	
-	tx.begin();
-	
-	Paciente existe = ent.find(Paciente.class, paciente.getIdpaciente());
-	
-	if(existe != null) {
-		existe.setNome(paciente.getNome());
-		existe.setAltura(paciente.getAltura());
-		existe.setSexo(paciente.getSexo());
-		existe.setCpf(paciente.getCpf());
-		existe.setIdade(paciente.getIdade());
-		existe.setEmail(paciente.getEmail());
-		existe.setPeso(paciente.getPeso());
-		existe.setTelefone(paciente.getTelefone());
-		existe.setRua(paciente.getRua());
-		existe.setNumero(paciente.getNumero());
-		existe.setCep(paciente.getCep());
-		
-		ent.merge(paciente);
-		tx.commit();
-		ent.close();
-		return true;
+	public boolean alterarPaciente(Paciente paciente) {
+		EntityManager ent = JpaUtil.getEntityManager();
+		EntityTransaction tx = ent.getTransaction();
+
+		tx.begin();
+
+		Paciente existe = ent.find(Paciente.class, paciente.getIdpaciente());
+
+		if (existe != null) {
+			existe.setNome(paciente.getNome());
+			existe.setAltura(paciente.getAltura());
+			existe.setSexo(paciente.getSexo());
+			existe.setCpf(paciente.getCpf());
+			existe.setIdade(paciente.getIdade());
+			existe.setEmail(paciente.getEmail());
+			existe.setPeso(paciente.getPeso());
+			existe.setTelefone(paciente.getTelefone());
+			existe.setRua(paciente.getRua());
+			existe.setNumero(paciente.getNumero());
+			existe.setCep(paciente.getCep());
+
+			ent.merge(paciente);
+			tx.commit();
+			ent.close();
+			return true;
+		} else {
+			return false;
+		}
 	}
-	else {
-	return false;
-	}
-}
 
 //Remover Paciente
 
-public boolean removerPaciente(Paciente paciente) {
-	EntityManager ent = JpaUtil.getEntityManager();
-	EntityTransaction tx = ent.getTransaction();
-	
-	tx.begin();
-	
-	Paciente existe = ent.find(Paciente.class, paciente.getIdpaciente());
-	
-	if(existe != null) {
-		
-		ent.remove(existe);
-		tx.commit();
-		ent.close();
-		return true;
-	}	
-else {
-	return false;
-}
-	
-}
+	public boolean removerPaciente(Paciente paciente) {
+		EntityManager ent = JpaUtil.getEntityManager();
+		EntityTransaction tx = ent.getTransaction();
 
+		tx.begin();
+
+		Paciente existe = ent.find(Paciente.class, paciente.getIdpaciente());
+
+		if (existe != null) {
+
+			ent.remove(existe);
+			tx.commit();
+			ent.close();
+			return true;
+		} else {
+			return false;
+		}
+
+	}
 
 //Pesquisar Paciente
 
-public List<Paciente> pesquisarPaciente(Paciente paciente) {
-String sql = "from Paciente p where 1=1" + montarWhere(paciente);	
-return null;	
-}
+	public List<Paciente> pesquisarPaciente(Paciente paciente) {
+		String sql = "from Paciente p where 1=1" + montarWhere(paciente);
+		return null;
+	}
 
 //String montarWhere
-private String montarWhere(Paciente paciente) {
-String where = "1";	
-	
-	if (paciente.getCpf() != null && !paciente.getNome().isEmpty()) {
-		where += "and p.cpf = " + paciente.getCpf() + "'";
-	}else {
-		if (paciente.getNome() != null && !paciente.getNome().isEmpty()) {
-			where += "and p.nome like '%" + paciente.getNome() + "%'";
+	private String montarWhere(Paciente paciente) {
+		String where = "1";
+
+		if (paciente.getCpf() != null && !paciente.getNome().isEmpty()) {
+			where += "and p.cpf = " + paciente.getCpf() + "'";
+		} else {
+			if (paciente.getNome() != null && !paciente.getNome().isEmpty()) {
+				where += "and p.nome like '%" + paciente.getNome() + "%'";
+			}
+			if (paciente.getIdade() > 0) {
+				where += "and p.idade = " + paciente.getIdade();
+			}
+			if (paciente.getSexo() != null && !paciente.getSexo().isEmpty()) {
+				where += "and p.sexo like '%" + paciente.getSexo() + "'";
+			}
 		}
-		if(paciente.getIdade() > 0) {
-			where += "and p.idade = " + paciente.getIdade();
-		}
-		if (paciente.getSexo() != null && !paciente.getSexo().isEmpty()) {
-			where += "and p.sexo like '%" + paciente.getSexo() + "'";
- 	}
-}
-return null;
-}
+		return null;
+	}
 
 //Inserir atendimento
 
-public boolean inserirAtendimento(Atendimento atendimento) {
-	EntityManager ent = JpaUtil.getEntityManager();
-	EntityTransaction tx = ent.getTransaction();
-	
-	tx.begin();
-	ent.merge(atendimento);
-	tx.commit();
-	ent.close();
-	
-	return true;
-	
-}
+	public boolean inserirAtendimento(Atendimento atendimento) {
+		EntityManager ent = JpaUtil.getEntityManager();
+		EntityTransaction tx = ent.getTransaction();
 
-//Alterar Atendimento
-
-public boolean alterarAtendimento(Atendimento atendimento) {
-	EntityManager ent = JpaUtil.getEntityManager();
-	EntityTransaction tx = ent.getTransaction();
-	
-	tx.begin();
-	
-	Atendimento existe = ent.find(Atendimento.class, atendimento.getIdatendimento());
-	
-	
-	if(existe != null) {
-		existe.setDta(atendimento.getDta());
-		existe.setCid(atendimento.getCid());
-		existe.setAvaliacao(atendimento.getAvaliacao());
-		existe.setEvolucao(atendimento.getEvolucao());
-		existe.setPrescriao(atendimento.getPrescriao());
-		existe.setObs(atendimento.getObs());
-		existe.setConduta(atendimento.getConduta());
-		
-		
+		tx.begin();
 		ent.merge(atendimento);
 		tx.commit();
 		ent.close();
+
 		return true;
+
 	}
-	else {
-	return false;
+
+//Alterar Atendimento
+
+	public boolean alterarAtendimento(Atendimento atendimento) {
+		EntityManager ent = JpaUtil.getEntityManager();
+		EntityTransaction tx = ent.getTransaction();
+
+		tx.begin();
+
+		Atendimento existe = ent.find(Atendimento.class, atendimento.getIdatendimento());
+
+		if (existe != null) {
+			existe.setDta(atendimento.getDta());
+			existe.setCid(atendimento.getCid());
+			existe.setAvaliacao(atendimento.getAvaliacao());
+			existe.setEvolucao(atendimento.getEvolucao());
+			existe.setPrescriao(atendimento.getPrescriao());
+			existe.setObs(atendimento.getObs());
+			existe.setConduta(atendimento.getConduta());
+
+			ent.merge(atendimento);
+			tx.commit();
+			ent.close();
+			return true;
+		} else {
+			return false;
+		}
 	}
-}
 
 //Remover Atendimento
 
-public boolean removerAtendimento(Atendimento atendimento) {
-	EntityManager ent = JpaUtil.getEntityManager();
-	EntityTransaction tx = ent.getTransaction();
-	
-	tx.begin();
-	
-	Atendimento existe = ent.find(Atendimento.class, atendimento.getIdatendimento());
-	
-	if(existe != null) {
-		
-		ent.remove(atendimento);
-		tx.commit();
-		ent.close();
-		return true;
-	}	
-else {
-	return false;
-}
-}
+	public boolean removerAtendimento(Atendimento atendimento) {
+		EntityManager ent = JpaUtil.getEntityManager();
+		EntityTransaction tx = ent.getTransaction();
+
+		tx.begin();
+
+		Atendimento existe = ent.find(Atendimento.class, atendimento.getIdatendimento());
+
+		if (existe != null) {
+
+			ent.remove(atendimento);
+			tx.commit();
+			ent.close();
+			return true;
+		} else {
+			return false;
+		}
+	}
 //Pesquisar Atendimento
 
-/*public List<Atendimento> pesquisarAtendimento(Atendimento atendimento) {
-String sql = "from atendimento p where 1=1" + montarWhere(atendimento);	
-return null;	
-}
-
-//String montarWhere
-private String montarWhere(Atendimento atendimento) {
-String where = "";	
-	
-	if (atendimento.getIdatendimento() != null && !atendimento.getNome().isEmpty()) {
-		where += "and p.cpf = " + paciente.getCpf() + "'";
-	}else {
-		if (paciente.getNome() != null && !paciente.getNome().isEmpty()) {
-			where += "and p.nome like '%" + paciente.getNome() + "%'";
-		}
-		if(paciente.getIdade() > 0) {
-			where += "and p.idade = " + paciente.getIdade();
-		}
-		if (paciente.getSexo() != null && !paciente.getSexo().isEmpty()) {
-			where += "and p.sexo like '%" + paciente.getSexo() + "'";
-	}
-}
-return null;
-}*/
+	/*
+	 * public List<Atendimento> pesquisarAtendimento(Atendimento atendimento) {
+	 * String sql = "from atendimento p where 1=1" + montarWhere(atendimento);
+	 * return null; }
+	 * 
+	 * //String montarWhere private String montarWhere(Atendimento atendimento) {
+	 * String where = "";
+	 * 
+	 * if (atendimento.getIdatendimento() != null &&
+	 * !atendimento.getNome().isEmpty()) { where += "and p.cpf = " +
+	 * paciente.getCpf() + "'"; }else { if (paciente.getNome() != null &&
+	 * !paciente.getNome().isEmpty()) { where += "and p.nome like '%" +
+	 * paciente.getNome() + "%'"; } if(paciente.getIdade() > 0) { where +=
+	 * "and p.idade = " + paciente.getIdade(); } if (paciente.getSexo() != null &&
+	 * !paciente.getSexo().isEmpty()) { where += "and p.sexo like '%" +
+	 * paciente.getSexo() + "'"; } } return null; }
+	 */
 
 //Inserir Profissional	
 
-public boolean inserirProfissional(Profissional profissional) {
-	EntityManager ent = JpaUtil.getEntityManager();
-	EntityTransaction tx = ent.getTransaction();
-	
-	tx.begin();
-	ent.merge(profissional);
-	tx.commit();
-	ent.close();
-	
-	return true;
-}
+	public boolean inserirProfissional(Profissional profissional) {
+		EntityManager ent = JpaUtil.getEntityManager();
+		EntityTransaction tx = ent.getTransaction();
 
-//Alterar Profissional
-
-public boolean alterarProfissional(Profissional profissional) {
-	EntityManager ent = JpaUtil.getEntityManager();
-	EntityTransaction tx = ent.getTransaction();
-	
-	tx.begin();
-	
-	Profissional existe = ent.find(Profissional.class, profissional.getCpf());
-	
-	if(existe != null) {
-		existe.setNome(profissional.getNome());
-		existe.setCpf(profissional.getCpf());
-		existe.setEmail(profissional.getEmail());
-		existe.setTelefone(profissional.getTelefone());
-		existe.setArea(profissional.getArea());
-		existe.setNumeroRegistro(profissional.getNumeroRegistro());
-		existe.setTipoProfissional(profissional.getTipoProfissional());
-		existe.setSenha(profissional.getSenha());
-		
+		tx.begin();
 		ent.merge(profissional);
 		tx.commit();
 		ent.close();
+
 		return true;
 	}
-	else {
-	return false;
+
+//Alterar Profissional
+
+	public boolean alterarProfissional(Profissional profissional) {
+		EntityManager ent = JpaUtil.getEntityManager();
+		EntityTransaction tx = ent.getTransaction();
+
+		tx.begin();
+
+		Profissional existe = ent.find(Profissional.class, profissional.getCpf());
+
+		if (existe != null) {
+			existe.setNome(profissional.getNome());
+			existe.setCpf(profissional.getCpf());
+			existe.setEmail(profissional.getEmail());
+			existe.setTelefone(profissional.getTelefone());
+			existe.setArea(profissional.getArea());
+			existe.setNumeroRegistro(profissional.getNumeroRegistro());
+			existe.setTipoProfissional(profissional.getTipoProfissional());
+			existe.setSenha(profissional.getSenha());
+
+			ent.merge(profissional);
+			tx.commit();
+			ent.close();
+			return true;
+		} else {
+			return false;
+		}
 	}
-}
 
 //Remover Pofissional
 
-public boolean removerProfissional(Profissional profissional) {
-	EntityManager ent = JpaUtil.getEntityManager();
-	EntityTransaction tx = ent.getTransaction();
-	
-	tx.begin();
-	
-	Profissional existe = ent.find(Profissional.class, profissional.getCpf());
-	
-	if(existe != null) {
-		
-		ent.remove(profissional);
-		tx.commit();
-		ent.close();
-		return true;
-	}	
-else {
-	return false;
-}
-	
-}
+	public boolean removerProfissional(Profissional profissional) {
+		EntityManager ent = JpaUtil.getEntityManager();
+		EntityTransaction tx = ent.getTransaction();
+
+		tx.begin();
+
+		Profissional existe = ent.find(Profissional.class, profissional.getCpf());
+
+		if (existe != null) {
+
+			ent.remove(profissional);
+			tx.commit();
+			ent.close();
+			return true;
+		} else {
+			return false;
+		}
+
+	}
 
 //Pesquisar Profissional
 
-/*public List<Atendimento> pesquisarAtendimento(Atendimento atendimento) {
-String sql = "from atendimento p where 1=1" + montarWhere(atendimento);	
-return null;	
-}
-
-//String montarWhere
-private String montarWhere(Atendimento atendimento) {
-String where = "";	
-	
-	if (atendimento.getIdatendimento() != null && !atendimento.getNome().isEmpty()) {
-		where += "and p.cpf = " + paciente.getCpf() + "'";
-	}else {
-		if (paciente.getNome() != null && !paciente.getNome().isEmpty()) {
-			where += "and p.nome like '%" + paciente.getNome() + "%'";
-		}
-		if(paciente.getIdade() > 0) {
-			where += "and p.idade = " + paciente.getIdade();
-		}
-		if (paciente.getSexo() != null && !paciente.getSexo().isEmpty()) {
-			where += "and p.sexo like '%" + paciente.getSexo() + "'";
-	}
-}
-return null;
-}*/
+	/*
+	 * public List<Atendimento> pesquisarAtendimento(Atendimento atendimento) {
+	 * String sql = "from atendimento p where 1=1" + montarWhere(atendimento);
+	 * return null; }
+	 * 
+	 * //String montarWhere private String montarWhere(Atendimento atendimento) {
+	 * String where = "";
+	 * 
+	 * if (atendimento.getIdatendimento() != null &&
+	 * !atendimento.getNome().isEmpty()) { where += "and p.cpf = " +
+	 * paciente.getCpf() + "'"; }else { if (paciente.getNome() != null &&
+	 * !paciente.getNome().isEmpty()) { where += "and p.nome like '%" +
+	 * paciente.getNome() + "%'"; } if(paciente.getIdade() > 0) { where +=
+	 * "and p.idade = " + paciente.getIdade(); } if (paciente.getSexo() != null &&
+	 * !paciente.getSexo().isEmpty()) { where += "and p.sexo like '%" +
+	 * paciente.getSexo() + "'"; } } return null; }
+	 */
 
 //Inserir Processo	
 
-public boolean inserirProcesso(Processo processo) {
-	EntityManager ent = JpaUtil.getEntityManager();
-	EntityTransaction tx = ent.getTransaction();
-	
-	tx.begin();
-	ent.merge(processo);
-	tx.commit();
-	ent.close();
-	
-	return true;
-	
-}
-//Alterar Processo
+	public boolean inserirProcesso(Processo processo) {
+		EntityManager ent = JpaUtil.getEntityManager();
+		EntityTransaction tx = ent.getTransaction();
 
-public boolean alterarProcesso(Processo processo) {
-	EntityManager ent = JpaUtil.getEntityManager();
-	EntityTransaction tx = ent.getTransaction();
-	
-	tx.begin();
-	
-	Processo existe = ent.find(Processo.class, processo.getNome());
-	
-	if(existe != null) {
-		existe.setNome(processo.getNome());
-		existe.setDescricao(processo.getDescricao());
-		
-		
+		tx.begin();
 		ent.merge(processo);
 		tx.commit();
 		ent.close();
+
 		return true;
+
 	}
-	else {
-	return false;
+//Alterar Processo
+
+	public boolean alterarProcesso(Processo processo) {
+		EntityManager ent = JpaUtil.getEntityManager();
+		EntityTransaction tx = ent.getTransaction();
+
+		tx.begin();
+
+		Processo existe = ent.find(Processo.class, processo.getNome());
+
+		if (existe != null) {
+			existe.setNome(processo.getNome());
+			existe.setDescricao(processo.getDescricao());
+
+			ent.merge(processo);
+			tx.commit();
+			ent.close();
+			return true;
+		} else {
+			return false;
+		}
 	}
-}
 
 //Remover Paciente
 
-public boolean removerPaciente(Processo processo) {
-	EntityManager ent = JpaUtil.getEntityManager();
-	EntityTransaction tx = ent.getTransaction();
-	
-	tx.begin();
-	
-	Processo existe = ent.find(Processo.class, processo.getNome());
-	
-	if(existe != null) {
-		
-		ent.remove(processo);
-		tx.commit();
-		ent.close();
-		return true;
-	}	
-else {
-	return false;
-}
-	
-}
+	public boolean removerPaciente(Processo processo) {
+		EntityManager ent = JpaUtil.getEntityManager();
+		EntityTransaction tx = ent.getTransaction();
+
+		tx.begin();
+
+		Processo existe = ent.find(Processo.class, processo.getNome());
+
+		if (existe != null) {
+
+			ent.remove(processo);
+			tx.commit();
+			ent.close();
+			return true;
+		} else {
+			return false;
+		}
+
+	}
 
 //Pesquisar Paciente
 
-/*public List<Paciente> pesquisarPaciente(Paciente paciente) {
-String sql = "from Paciente p where 1=1" + montarWhere(paciente);	
-return null;	
-}
-
-//String montarWhere
-private String montarWhere(Paciente paciente) {
-String where = "";	
-	
-	if (paciente.getCpf() != null && !paciente.getNome().isEmpty()) {
-		where += "and p.cpf = " + paciente.getCpf() + "'";
-	}else {
-		if (paciente.getNome() != null && !paciente.getNome().isEmpty()) {
-			where += "and p.nome like '%" + paciente.getNome() + "%'";
-		}
-		if(paciente.getIdade() > 0) {
-			where += "and p.idade = " + paciente.getIdade();
-		}
-		if (paciente.getSexo() != null && !paciente.getSexo().isEmpty()) {
-			where += "and p.sexo like '%" + paciente.getSexo() + "'";
-	}
-}
-return null;
-}
-*/
+	/*
+	 * public List<Paciente> pesquisarPaciente(Paciente paciente) { String sql =
+	 * "from Paciente p where 1=1" + montarWhere(paciente); return null; }
+	 * 
+	 * //String montarWhere private String montarWhere(Paciente paciente) { String
+	 * where = "";
+	 * 
+	 * if (paciente.getCpf() != null && !paciente.getNome().isEmpty()) { where +=
+	 * "and p.cpf = " + paciente.getCpf() + "'"; }else { if (paciente.getNome() !=
+	 * null && !paciente.getNome().isEmpty()) { where += "and p.nome like '%" +
+	 * paciente.getNome() + "%'"; } if(paciente.getIdade() > 0) { where +=
+	 * "and p.idade = " + paciente.getIdade(); } if (paciente.getSexo() != null &&
+	 * !paciente.getSexo().isEmpty()) { where += "and p.sexo like '%" +
+	 * paciente.getSexo() + "'"; } } return null; }
+	 */
 }
